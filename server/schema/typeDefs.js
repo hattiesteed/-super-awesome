92b81e935b7f5348557a73f970fe838e996a605e
CaptainFlopp3y <barrancofabian@gmail.com>
2023-08-05 00:19:26 -0600

    type def starting

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -5,31 +5,12 @@ const typeDefs = gql`
         _id: ID!
         username: String!
         email: String!
-        bookCount: Int
-        savedBooks: [Book]
+
     }
 
     type Auth {
         token: ID!
         user: User
-    }
-
-    type Book {
-        bookId: ID!
-        authors: [String]
-        description: String
-        title: String
-        image: String
-        link: String
-    }
-
-    input InputBook {
-        bookId: String!
-        authors: [String]
-        title: String
-        description: String
-        image: String
-        link: String
     }
 
     type Query {
@@ -39,8 +20,6 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(newBook: InputBook!): User
-        removeBook(bookId: String!): User
     }
 `;
 
