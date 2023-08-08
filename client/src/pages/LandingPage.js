4572db4496845543f4119d169e13ef706622e4b4
mkidee <mckaymemmott@gmail.com>
2023-08-07 21:02:11 -0600

    hoop zone

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -3,23 +3,16 @@ import nbaPlayers from '../assets/nbaPlayers.jpg'
 const LandingPage = () => {
     return (
         <main className='welcomeMain'>
-            <h1 id='welcomeLog'> Welcome to "please name me"! </h1>
             <br></br>
-            <h2>Please log in / sign up to continue!</h2>
+            <h1 id='welcomeLog'> Welcome to Hoop Zone! </h1>
+            <br></br>
+            <h2>Please log in or sign up to continue!</h2>
+
             <br></br>
             <br></br>
             <br></br>
 
             <img src={nbaPlayers} alt='big name nba players' id='playersPic' />
-
-
-
-
-
-
-
-
-
 
             <footer id='welcomeFoot'>
             <p>Created by: Hattie Steed, Fabian Barranco, Kelton Sterett, Mckay Memmott</p>
