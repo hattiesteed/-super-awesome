6456fbbfe3f6163e5a3828189131f771bc1eac83
mkidee <mckaymemmott@gmail.com>
2023-08-07 21:32:05 -0600

    profile drop down for favorite team started

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -4,22 +4,33 @@ import { useQuery } from '@apollo/client';
 import Auth from '../utils/auth';
 
 const ProfilePage = () => {
-    const { loading, error, data, refetch } = useQuery(GET_ME);
-    const user = data?.me || data?.user || {};
-    // navigate to personal profile page if username is yours
-    if (!Auth.loggedIn()) {
-        return <Navigate to="/" />;
-    }
+    // const { loading, error, data, refetch } = useQuery(GET_ME);
+    // const user = data?.me || data?.user || {};
+    // // navigate to personal profile page if username is yours
+    // if (!Auth.loggedIn()) {
+    //     return <Navigate to="/" />;
+    // }
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+    // if (loading) {
+    //     return <div>Loading...</div>;
+    // }
     return (
         <main>
 
+            <h1>testing</h1>
 
+            <br></br>
+            <br></br>
+            <br></br>
 
+            <h3>Select your favorite NBA team:</h3>
 
+            <label for='teams'>Select a team:</label>
+
+            <select name='teams' id='teams'>
+                <option value='lakers'>Los Angeles Lakers</option>
+                <option value='nugs'>Denver Nuggets</option>
+            </select>
 
 
         </main>
